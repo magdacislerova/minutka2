81e42e8c29a885f8b4c75f064e7ecb681c50bf67
magdacislerova <55711011+magdacislerova@users.noreply.github.com>
2020-07-29 22:21:51 +0200

    slightly different solution, deletes input field after starting countdown

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 'use strict';
 
-console.log('funguju!');
-
-const inputElm = document.querySelector('.time-input'); //vybírám pole, kam uživatel zadá číslo
-const alarmElm = document.querySelector('.alarm__seconds') //vybírám pole, kam se propisuje čas
+const inputElm = document.querySelector('.time-input');
+const buttonElm = document.getElementById('button');
+const secondsElm = document.querySelector('.alarm__seconds');
 const audioElm = document.querySelector('audio');
+const alarmElm = document.querySelector('.alarm');
 
-const countdown = (event) => {
-  let time = Number(inputElm.value); //převádím vzniklý řetězec na číslo a ukládám do proměnné time
-  alarmElm.textContent = time;
-  console.log(time);
-
+const countdown = () => {
+  let time = Number(inputElm.value);
+  secondsElm.innerHTML = time; /* tady je to kvůli zobrazení počátečního čísla odpočtu */
   const timerId = setInterval(() => {
-    time = time - 1;
-    alarmElm.textContent = time;
-    if (time === 0) {
-     alarmElm.className = 'alarm alarm--ring';
-     audioElm.play();
-     clearInterval(timerId);
-    };
+    if (time > 0) {
+      time -= 1;
+      secondsElm.innerHTML = time; /* tady aby to zobrazovalo odečítání */
+      console.log(time);
+    } else {
+      alarmElm.classList.add('alarm--ring');
+      audioElm.play();
+      clearTimeout(timerId);
+    }
   }, 1000);
-  
-}
+  inputElm.value = '';
+};
 
-document.querySelector('button').addEventListener('click', countdown);
+buttonElm.addEventListener('click', countdown);
 
-
-
+console.log('funguju!');
